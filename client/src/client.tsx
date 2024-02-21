de5f9d6ab1698b655f25fe16b24497f5218b6168
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-02-22 01:30:24 +0545

    feat: Auth0 authentication added

--- client/src/client.tsx
+++ client/src/client.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRouter from './AppRouter';
 import './global.css';
+import Auth0Providers from './auth/Auth0Provider';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
-      <AppRouter />
+      <Auth0Providers>
+        <AppRouter />
+      </Auth0Providers>
     </Router>
   </React.StrictMode>
 );
