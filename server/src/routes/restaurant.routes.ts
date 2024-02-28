cd75031a040a0d807a3d6e63a103e186591f0194
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-02-28 23:09:23 +0545

    feat: search api added

---
+++ server/src/routes/restaurant.routes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { param } from 'express-validator';
+import restaurantsControllers from '../controllers/restaurants.controllers';
+
+const router = express.Router();
+
+router.get(
+  '/search/:city',
+  param('city')
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage('City parameter must be a valid string'),
+  restaurantsControllers.searchRestaurant
+);
