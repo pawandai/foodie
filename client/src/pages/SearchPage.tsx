36fe2108f35eca02d880088875eb0a51b6772618
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-03-01 00:13:59 +0545

    feat: search functionality with pagination

--- client/src/pages/SearchPage.tsx
+++ client/src/pages/SearchPage.tsx
@@ -1,9 +1,110 @@
+import { useSearchRestaurants } from '@/api/RestaurantsApi';
+import CuisineFilter from '@/components/shared/CuisineFilter';
+import PaginationSelector from '@/components/shared/PaginationSelector';
+import SearchBar, { SearchForm } from '@/components/shared/SearchBar';
+import SearchResultCard from '@/components/shared/SearchResultCard';
+import SearchResultInfo from '@/components/shared/SearchResultInfo';
+import SortOptionDropdown from '@/components/shared/SortOptionDropdown';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+
+export type SearchState = {
+  searchQuery: string;
+  page: number;
+  selectedCuisines: string[];
+  sortOption: string;
+};
 
 const SearchPage = () => {
   const { city } = useParams();
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: '',
+    page: 1,
+    selectedCuisines: [],
+    sortOption: 'bestMatch',
+  });
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const { results, isLoading } = useSearchRestaurants(searchState, city);
 
-  return <span>User searched for {city}</span>;
+  const setSortOption = (sortOption: string) => {
+    setSearchState((prev) => ({
+      ...prev,
+      sortOption,
+      page: 1,
+    }));
+  };
+
+  const setSelectedCuisines = (selectedCuisines: string[]) => {
+    setSearchState((prev) => ({
+      ...prev,
+      selectedCuisines,
+      page: 1,
+    }));
+  };
+
+  const setPage = (page: number) => {
+    setSearchState((prev) => ({
+      ...prev,
+      page,
+    }));
+  };
+
+  const setSearchQuery = (searchFormData: SearchForm) => {
+    setSearchState((prev) => ({
+      ...prev,
+      searchQuery: searchFormData.searchQuery,
+      page: 1,
+    }));
+  };
+
+  const resetSearch = () => {
+    setSearchState((prev) => ({
+      ...prev,
+      searchQuery: '',
+      page: 1,
+    }));
+  };
+
+  if (isLoading) return <span>Loading...</span>;
+
+  if (!results?.data || !city) return <span>No results found</span>;
+
+  return (
+    <div className='grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5'>
+      <div id='cuisines-list'>
+        <CuisineFilter
+          selectedCuisines={searchState.selectedCuisines}
+          onChange={setSelectedCuisines}
+          isExpanded={isExpanded}
+          onExpandedClick={() => setIsExpanded((prev) => !prev)}
+        />
+      </div>
+      <div id='main-content' className='flex flex-col gap-5'>
+        <SearchBar
+          searchQuery={searchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeHolder='Search by Cuisine or Restaurant Name'
+          onReset={resetSearch}
+        />
+        <div className='flex justify-between flex-col gap-3 lg:flex-row'>
+          <SearchResultInfo total={results.pagination.total} city={city} />
+          <SortOptionDropdown
+            sortOption={searchState.sortOption}
+            onChange={(value) => setSortOption(value)}
+          />
+        </div>
+
+        {results.data.map((restaurant) => (
+          <SearchResultCard restaurant={restaurant} />
+        ))}
+        <PaginationSelector
+          page={results.pagination.page}
+          pages={results.pagination.pages}
+          onPageChange={setPage}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
