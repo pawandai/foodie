fab67cd566831c64c07c9aa2a02612eedb3917ac
Pawan Awasthi <pawanawasthi481@gmail.com>
2024-03-01 18:31:27 +0545

    restaurants detail endpoint added

--- src/controllers/restaurants.controllers.ts
+++ src/controllers/restaurants.controllers.ts
@@ -1,6 +1,21 @@
 import { Request, Response } from 'express';
 import Restaurant from '../models/restaurant.model';
 
+const getRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurantId = req.params.restaurantId;
+
+    const restaurant = await Restaurant.findById(restaurantId);
+    if (!restaurant)
+      return res.status(404).json({ message: 'Restaurant not found' });
+
+    res.json(restaurant);
+  } catch (error) {
+    console.log('error', error);
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
+
 const searchRestaurant = async (req: Request, res: Response) => {
   try {
     const city = req.params.city;
@@ -67,4 +82,4 @@ const searchRestaurant = async (req: Request, res: Response) => {
   }
 };
 
-export default { searchRestaurant };
+export default { searchRestaurant, getRestaurant };
